02834e0e86a4f174f9a639e9d83193177b6ce220
seandurban <seandurban10@gmail.com>
2020-05-21 18:04:54 +0100

    Moved validation to own method

--- src/agents/printAgent.js
+++ src/agents/printAgent.js
@@ -1,13 +1,23 @@
 const utils = require('./utils');
 
 async function resolve(printAgentDetails, data) {
-    if(printAgentDetails.options.message && typeof printAgentDetails.options.message == "string") {
+    if(validParameters(printAgentDetails)) {
         let output = utils.interpolateString(printAgentDetails.options.message, data);
         console.log(output);
     }
     else {
         console.error("Invalid PrintAgent format");
     }
+}
+
+function validParameters(printAgentDetails) {
+    if(printAgentDetails && printAgentDetails.options && printAgentDetails.options.message 
+        && typeof printAgentDetails.options.message == "string") {
+            return true;
+    }
+    
+    return false;
+
 }
 
 module.exports = {resolve};
